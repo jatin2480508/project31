db7b0264d1696c879e89a896b561b6f9daab24f0
jatin2480508 <72887584+jatin2480508@users.noreply.github.com>
2020-12-05 11:26:00 +0000

    Add files via upload

---
+++ Division.js
@@ -0,0 +1,22 @@
+class Divisions{
+    constructer(x, y, w, h){
+        var options ={
+
+            isStatic: true
+
+
+        }
+         this.body = Bodies.rectangle(x, y, w, h, options);
+         this.w = w;
+         this.h = h;
+         World.add(world, this.body);
+    }
+    display(){
+         var pos = this.body.position;
+         rectMode(CENTRE)
+         Fill("white");
+         rect(pos.x, pos.y ,this.w ,this.h);
+
+
+    }
+};
